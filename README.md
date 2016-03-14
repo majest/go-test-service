fcf18aa1d229dfe68f3e4d7c64fc2ba1520a5214
Artur Grabowski <majest.pl@gmail.com>
2016-03-14 16:12:17 +0000

    Added flags

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Test service serving simple string length endpoint using go-kit, GRPC and Consul
+
+```
+Usage of ./go-test-service:
+  -consulip string
+    	Consul node ip
+  -consulport int
+    	Consul node port (default 8500)
+  -ip string
+    	Service ip. Should be local ip if run locally (default "127.0.0.1")
+  -port int
+    	Service port (default 9090)
+```
--- main.go
+++ main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
 
-	"github.com/majest/go-test-service/consul"
+	"github.com/majest/go-microservice/consul"
 	"github.com/majest/go-test-service/handler"
 	"github.com/majest/go-test-service/pb"
 	"google.golang.org/grpc"
 )
 
+var serviceIP, consulIP string
+var servicePort, consulPort int
+
+func init() {
+	flag.StringVar(&serviceIP, "ip", "127.0.0.1", "Service ip. Should be local ip if run locally")
+	flag.IntVar(&servicePort, "port", 9090, "Service port")
+	flag.StringVar(&consulIP, "consulip", "", "Consul node ip")
+	flag.IntVar(&consulPort, "consulport", 8500, "Consul node port")
+	flag.Parse()
+}
+
 func main() {
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9090))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	g := grpc.NewServer()
-	consul.RegisterService("com.service.string")
+	consul.RegisterService("com.service.string", &consul.Config{
+		ServiceIp:   serviceIP,
+		ServicePort: servicePort,
+		NodeIp:      consulIP,
+		NodePort:    consulPort})
+
 	pb.RegisterStringsServer(g, new(handler.StringServer))
 	g.Serve(lis)
 }
